026a09a798b3afad1bf3c190f8acb1b9896f2c79
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-06 14:38:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,30 +4,24 @@ function init() {
 
   const input = document.querySelector('input')
 
-  document.body.addEventListener('keydown', onKeyDownHandler);
-  const alphabet = [65, 66, 67];
   let index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler);
+  
 
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-
-    for (var i = 0; i < code.length; i++){
-      if (key === code[index]){
-        index++;
-        console.log(index);
-
-        if (index === code.length){
-          alert("Hurray!");
-
-          index = 0;
-        }
-      }
-      else{
+ 
+    if (key === code[index]) {
+      index++;
+      console.log(index);
+   
+      if (index === code.length) {
+        alert("Hurray!");
+   
         index = 0;
       }
+    } else {
+      index = 0;
     }
-
-
-
   }
 }
